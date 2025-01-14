af7518541aa0c78818e732015eb3524e283817f3
PieMenard <110061246+PieMenard@users.noreply.github.com>
2025-01-14 09:11:59 -0400

    fix clashing styles

--- src/components/directory-item/directory-item.component.jsx
+++ src/components/directory-item/directory-item.component.jsx
@@ -1,17 +1,17 @@
-import './category-item.styles.scss';
+import './directory-item.styles.scss';
 
 import React from 'react';
 
 const CategoryItem = ({ category }) => {
   return (
-    <div className="category-container" key={category.id}>
+    <div className="directory-item-container" key={category.id}>
       <div
         className="background-image"
         style={{
           backgroundImage: `url(${category.imageUrl})`,
         }}
       />
-      <div className="category-body-container">
+      <div className="body">
         <h2>{category.title}</h2>
         <p>Shop Now</p>
       </div>
